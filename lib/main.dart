37a6ef69f83c607aa2ac97eb17d2bb555980570a
Pradyuman <76039454+pradyuman-hash@users.noreply.github.com>
2021-03-24 16:53:09 +0530

    theme extracted

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catalog/Pages/home_page.dart';
 import 'package:flutter_catalog/Pages/login_page.dart';
 import 'package:flutter_catalog/utils/routes.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_catalog/widgets/theme.dart';
+
 
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
-
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //  home: HomePage(),
-      themeMode: ThemeMode.system,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
       debugShowCheckedModeBanner: false,
-      darkTheme: ThemeData(
-        brightness: Brightness.dark),
+      themeMode: ThemeMode.system,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       initialRoute: Myroutes.homeRoute,
       routes: {
-        
         Myroutes.loginRoute:(context) => LoginPage(),
         Myroutes.homeRoute:(context)=>HomePage(),
-        //"/login": (context) => LoginPage()
       },
     );
   }
