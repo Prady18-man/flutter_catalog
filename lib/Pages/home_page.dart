1280115e368a68d8c277b5250a8cf48906702c7f
Pradyuman <76039454+pradyuman-hash@users.noreply.github.com>
2021-04-05 15:40:34 +0530

    Day15/16 complete

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,8 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
-import 'package:flutter_catalog/widgets/item_widget.dart';
 import 'dart:convert';
 
 class HomePage extends StatefulWidget {
@@ -12,7 +13,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final int days = 30;
-
   final String names = "Code";
   @override
   void initState() {
@@ -39,16 +39,26 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child:(CatalogModel.items !=null && CatalogModel.items.isNotEmpty) ? ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) => ItemWidget(
-              item: CatalogModel.items[index],
-            )
-          ,
-        ):
-        Center(
-          child: CircularProgressIndicator(),
-        ),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,crossAxisSpacing: 10),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                    child: GridTile(header: Container(decoration: BoxDecoration(color: Colors.deepOrange),
+                    padding: const EdgeInsets.all(12) ,
+                      child: Text(item.name,style: TextStyle(color: Colors.white))),
+                    footer: Container(padding: EdgeInsets.all(12),decoration: BoxDecoration(color: Colors.blueGrey),
+                    child: Text(item.price.toString(),style: TextStyle(color: Colors.white,))),
+                      child: Image.network(item.image)));
+                },
+                itemCount: CatalogModel.items.length)
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
